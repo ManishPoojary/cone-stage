5d69d4f3c97dbab9ec50176222f9f3229fd66d0b
MANISH POOJARY <manissh2327@gmail.com>
2020-08-24 23:58:51 +0530

    Added Home Page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  active: string = 'home';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  scrollToElement($element): void {
+    console.log($element);
+    $element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
   }
 
+  clicked(val) {
+    this.active = val;
+  }
 }
